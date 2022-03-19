aeb318fdda297c2fb630e336c2d6fa7a999b1aec
Rey Halsall <reyeduardohalsallquintero8@gmail.com>
2022-03-19 14:42:01 -0600

    Agregando el metodo Delete

---
+++ Depreciacion/AppCore/IServices/IActivoServices.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppCore.IServices
+{
+   public interface IActivoServices: IServices<Activo>
+    {
+        Activo GetById(int id);
+        bool Delete(int id);
+    }
+}
---
+++ Depreciacion/Domain/Interfaces/IActivoModel.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Entities;
+using Domain.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces
+{
+    public interface IActivoModel: IModel<Activo>
+    {
+        Activo GetById(int id);
+        bool Delete(int id);
+
+    }
+}
